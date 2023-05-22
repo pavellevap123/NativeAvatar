954e35d5b91dca2ed7a5eebda1f45fcebb316c4b
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2023-05-22 19:50:22 +0200

    Initial Commit

---
+++ NativeAvatar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NativeAvatar
+//
+//  Created by Pavel Paddubotski on 22.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
